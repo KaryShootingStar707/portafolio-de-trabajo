e0e137b1cc2a049e6bc545de04b73b578fc1c7a1
KaryShootingStar707 <patuluzo707@gmail.com>
2025-03-09 01:37:30 -0600

    mi sexto commit

---
+++ font.js
@@ -0,0 +1,8 @@
+const style = document.createElement("style");
+style.innerHTML = `
+  @font-face {
+    font-family: "Alfa_Slab_One";
+    src: url("estilo.font/Alfa_Slab_One/AlfaSlabOne-Regular.ttf") format("truetype");
+  }
+`;
+document.head.appendChild(style);
